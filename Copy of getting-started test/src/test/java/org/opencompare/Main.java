58cadf5804fdd8abe36055de6bca1ab972821231
Sandra VILLAFUERTE <sandra.isabel.villafuerte@gmail.com>
2015-10-05 12:01:47 +0200

    test commit

---
+++ Copy of getting-started test/src/test/java/org/opencompare/Main.java
@@ -0,0 +1,13 @@
+package org.opencompare;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		MyPCMPrinterTest test = new MyPCMPrinterTest();
+		test.testMyPCMPrinter();
+	}
+
+}
